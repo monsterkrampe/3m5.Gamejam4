dd7ac752a4e7da08caf24f327356ca7d4dfe8cd0
LostMekka <LostMekka@users.noreply.github.com>
2018-10-20 12:52:26 +0200

    added placeholder sprites, drawing player at fixed spot

--- core/src/gamejam4/game/KtxGame.kt
+++ core/src/gamejam4/game/KtxGame.kt
@@ -2,6 +2,7 @@ package gamejam4.game
 
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import ktx.app.KtxGame
@@ -9,6 +10,10 @@ import ktx.app.KtxScreen
 import ktx.graphics.use
 
 class GameplayScreen : KtxScreen {
+    private val playerSprite = Texture("player.png")
+    private val zombieSprite = Texture("zombie.png")
+    private val groundSprite = Texture("ground/default.png")
+
     private val font = BitmapFont()
     private val batch = SpriteBatch().apply {
         color = Color.WHITE
@@ -17,6 +22,7 @@ class GameplayScreen : KtxScreen {
     override fun render(delta: Float) {
         batch.use {
             font.draw(it, "Hello Kotlin!", 100f, 100f)
+            it.draw(playerSprite, 300f, 300f)
         }
     }
 
