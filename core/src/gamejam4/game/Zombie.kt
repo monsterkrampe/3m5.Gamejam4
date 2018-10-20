24178cb3af53ed05f71b79262b861f69c2478f6f
monsterkrampe <lukas.gerlach73@t-online.de>
2018-10-20 17:22:42 +0200

    zombies follow player now

--- core/src/gamejam4/game/Zombie.kt
+++ core/src/gamejam4/game/Zombie.kt
@@ -4,8 +4,11 @@ import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.scenes.scene2d.actions.Actions
+import com.badlogic.gdx.scenes.scene2d.actions.Actions.*
+import ktx.math.vec2
 
-class Zombie(x: Float, y: Float) : Actor() {
+class Zombie(x: Float, y: Float, player: Player) : Actor() {
     private val sprite = Sprite(Texture("zombie.png"))
     val speed: Float = 1f
     var actionInProgress = false
@@ -17,6 +20,25 @@ class Zombie(x: Float, y: Float) : Actor() {
         setBounds(0f, 0f, 1f, 1f)
         setOrigin(0.5f, 0.5f)
         setPosition(x, y)
+
+
+
+        addAction(forever(Actions.run {
+            if (actionInProgress) return@run
+
+            val distance = vec2(this.x, this.y).dst(vec2(player.x, player.y))
+
+            val moveAction = sequence(moveTo(player.x, player.y, distance / speed), Actions.run { actionInProgress = false})
+            val attackAction = sequence(delay(2f), Actions.run { println("attack") }, Actions.run { actionInProgress = false})
+
+            actionInProgress = true
+
+            if (distance < 0.1f) {
+                addAction(attackAction)
+            } else {
+                addAction(parallel(moveAction, sequence(delay(0.5f), Actions.run { removeAction(moveAction); actionInProgress = false })))
+            }
+        }))
     }
 
     override fun draw(batch: Batch, parentAlpha: Float) {
--- core/src/gamejam4/game/ZombieManager.kt
+++ core/src/gamejam4/game/ZombieManager.kt
@@ -1,35 +1,21 @@
 package gamejam4.game
 
 import com.badlogic.gdx.scenes.scene2d.Actor
-import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction
+import com.badlogic.gdx.scenes.scene2d.actions.Actions
+import com.badlogic.gdx.scenes.scene2d.actions.Actions.*
 import ktx.math.vec2
 import java.util.*
 
 class ZombieManager {
     val zombies = mutableListOf<Zombie>()
 
-    fun spawnZombieNear(x: Float, y: Float) : Actor {
+    fun spawnZombieNear(player: Player) : Actor {
         val randomMinusX = if (Random().nextBoolean()) 1 else -1
         val randomMinusY = if (Random().nextBoolean()) 1 else -1
         val randomX = Random().nextFloat() * 3
         val randomY = Random().nextFloat() * 3
-        val zombie = Zombie(x + randomMinusX * (1f + randomX), y + randomMinusY * (1f + randomY))
+        val zombie = Zombie(player.x + randomMinusX * (1f + randomX), player.y + randomMinusY * (1f + randomY), player)
         zombies.add(zombie)
         return zombie
-    }
-
-    fun attack(x: Float, y: Float) {
-        zombies.forEach {
-            it.clearActions()
-            val distance = vec2(it.x, it.y).dst(vec2(x, y))
-            if (distance < 0.5f) {
-                println("attack")
-            } else {
-                val moveAction = MoveToAction()
-                moveAction.setPosition(x, y)
-                moveAction.duration = distance / it.speed
-                it.addAction(moveAction)
-            }
-        }
     }
 }
