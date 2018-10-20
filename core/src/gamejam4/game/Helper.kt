bbe87cd3cad918f2437ad6da1c7ad5b66250f930
LostMekka <LostMekka@users.noreply.github.com>
2018-10-20 18:00:42 +0200

    made int point helpers more beautiful <3

--- core/src/gamejam4/game/Helper.kt
+++ core/src/gamejam4/game/Helper.kt
@@ -1,7 +1,20 @@
 package gamejam4.game
 
-data class PointI(val x: Int, val y: Int)
+data class PointI(val x: Int, val y: Int) {
+    operator fun rangeTo(endInclusive: PointI) = RectI(
+            x,
+            y,
+            endInclusive.x,
+            endInclusive.y
+    )
+}
 
-operator fun PointI.rangeTo(endInclusive: PointI) = RectI(this, endInclusive)
-
-data class RectI(val start: PointI, val endInclusive: PointI)
+data class RectI(
+        val startY: Int,
+        val startX: Int,
+        val endInclusiveX: Int,
+        val endInclusiveY: Int
+) {
+    val start by lazy { PointI(startX, startY) }
+    val endInclusive by lazy { PointI(endInclusiveX, endInclusiveY) }
+}
