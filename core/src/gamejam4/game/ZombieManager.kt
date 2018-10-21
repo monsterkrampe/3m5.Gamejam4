eb96a339e17a18bd12f840083894564ac40d519c
monsterkrampe <lukas.gerlach73@t-online.de>
2018-10-21 16:38:40 +0200

    improve spawn positions

--- core/src/gamejam4/game/ZombieManager.kt
+++ core/src/gamejam4/game/ZombieManager.kt
@@ -3,18 +3,19 @@ package gamejam4.game
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.scenes.scene2d.Actor
+import ktx.math.vec2
 import java.util.*
 
 class ZombieManager(val timer: Timer) {
     private val sprites = (1..6).map { Sprite(Texture("zombie$it.png")) }
 
     fun spawnZombieNear(player: Player): Actor {
-        val randomMinusX = if (Random().nextBoolean()) 1 else -1
-        val randomMinusY = if (Random().nextBoolean()) 1 else -1
-        val randomX = Random().nextFloat() * 5
-        val randomY = Random().nextFloat() * 5
-        val x = player.x + randomMinusX * (11f + randomX)
-        val y = player.y + randomMinusY * (6f + randomY)
+        val randomAngle = Random().nextInt(360).toFloat()
+        val distanceVector = vec2(15f)
+        distanceVector.setAngle(randomAngle)
+
+        val x = player.x + distanceVector.x
+        val y = player.y + distanceVector.y
         return if (Random().nextFloat() < 0.8) {
             DefaultZombie(x, y, sprites, player, timer)
         } else {
